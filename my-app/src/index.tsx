1c75fedef71a0dbb45adff179eb04a98137620af
yeramm <213187@jnu.ac.kr>
2024-07-04 20:20:28 +0900

    chore: prettier 및 craco 적용
    prettier을 적용하고craco를 적용함

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,9 +1,7 @@
-import '@/styles';
-
+import './styles'; // 상대 경로로 수정
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from '@/App';
+import App from './App'; // 상대 경로로 수정
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
